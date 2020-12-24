07a8d564f0cdf5acb2b6def611908dbec5df5f04
Viet Nam <74956696+DiAnTTA@users.noreply.github.com>
2020-12-24 21:05:46 +0700

    Update makefile.sh

--- makefile.sh
+++ makefile.sh
@@ -5,7 +5,21 @@ echo "Making titles..."
 TIME_STAMP=$(date +'%d %b %Y %H:%M')
 VERSION=$(date +'%y%m%d%H%M')
 LC_NUMERIC="en_US.UTF-8"
-RULE=$(printf "%'.3d\n" $(cat source/adserversVN.txt | grep -v '!' | wc -l))
 RULE_VN=$(printf "%'.3d\n" $(cat source/adserversVN.txt | grep -v '!' | wc -l))
 
 # update titles
+sed -e "s/_time_stamp_/$TIME_STAMP/g" -e "s/_version_/$VERSION/g" -e "s/_rule_vn_/$RULE_VN/g" tmp/title-adserverVN.txt > tmp/title-adserverVN.tmp
+
+echo "Creating adserver file..."
+# create temp adserver files
+cat source/adserversVN.txt | grep -v '!' | awk '{print $1}' >> tmp/adserversVN.tmp
+cat source/exceptions.txt | grep -v '!' |awk '{print $1}' >> tmp/exceptions.tmp
+
+# create adserver files
+cat tmp/adserversVN.tmp | awk '{print "||"$1"^"}' >> tmp/adserversVN-rule.tmp
+cat tmp/exceptions.tmp | awk '{print "@@||"$1"^|"}' >> tmp/adserversVN-rule.tmp
+cat tmp/adserversVN.tmp | awk '{print "*"$1" = 0.0.0.0"}' >> tmp/adserversVN-config.tmp
+
+echo "Creating rule file..."
+# create rule
+
